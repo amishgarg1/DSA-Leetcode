075825511762dcd396997e2d01fe6317632bc061
Amish Garg <amishgarg2205@gmail.com>
2026-06-01 10:39:31 +0530

    Add solution: 877. Stone Game

---
+++ 877. Stone Game/877. Stone Game.cpp
@@ -0,0 +1,14 @@
+/*
+ * Problem: 877. Stone Game
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/stone-game/submissions/2018713100/
+ * Language: cpp
+ * Date: 2026-06-01
+ */
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
