7dfa5bea0cfcefee7684d5488db04cfc41e6f152
Amish Garg <amishgarg2205@gmail.com>
2026-06-01 10:46:41 +0530

    Update solution: 81. Search in Rotated Sorted Array II

--- 81. Search in Rotated Sorted Array II/81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II/81. Search in Rotated Sorted Array II.cpp
@@ -1,7 +1,7 @@
 /*
  * Problem: 81. Search in Rotated Sorted Array II
  * Difficulty: Medium
- * Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/submissions/2018719095/
+ * Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/submissions/2018719691/
  * Language: cpp
  * Date: 2026-06-01
  */
@@ -10,7 +10,7 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int n =nums.size();
-        for(int i=0; i<n-1; i++){
+        for(int i=0; i<n; i++){
             if(nums[i]==target){
                 return true;
 
