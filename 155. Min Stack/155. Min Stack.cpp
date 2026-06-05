b5c4729a16032e2a8f5805e98981f5a97ede3155
Amish Garg <amishgarg2205@gmail.com>
2026-06-05 09:26:30 +0530

    Add solution: 155. Min Stack

---
+++ 155. Min Stack/155. Min Stack.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem: 155. Min Stack
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/min-stack/
+ * Language: cpp
+ * Date: 2026-06-05
+ */
+
+class MinStack {
+public:
+stack<int> st;
+stack<int> minSt;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty() || val<=minSt.top()){
+            minSt.push(val);
+        }
+    }
+    
+    void pop() {
+        if(st.top()== minSt.top()){
+            minSt.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
