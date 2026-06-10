f49b2a129fbb65d315610e2c20a2d4821ac92648
Amish Garg <amishgarg2205@gmail.com>
2026-06-10 10:56:27 +0530

    Add solution: 96. Unique Binary Search Trees

---
+++ 96. Unique Binary Search Trees/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: 96. Unique Binary Search Trees
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/unique-binary-search-trees/submissions/2028207525/
+ * Language: cpp
+ * Date: 2026-06-10
+ */
+
+class Solution {
+public:
+    int numTrees(int n) {
+        long long fact=1;
+        for(int i=0; i<n; i++){
+            fact*=(2*n-i);
+            fact=fact/(i+1);
+        }
+        return fact/(n+1);
+    }
+};
