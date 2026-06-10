71fd753d65120026e0d471030b3afc9019dada0a
Amish Garg <amishgarg2205@gmail.com>
2026-06-10 09:21:55 +0530

    Add solution: 42. Trapping Rain Water

---
+++ 42. Trapping Rain Water/42. Trapping Rain Water.cpp
@@ -0,0 +1,29 @@
+/*
+ * Problem: 42. Trapping Rain Water
+ * Difficulty: Hard
+ * Link: https://leetcode.com/problems/trapping-rain-water/
+ * Language: cpp
+ * Date: 2026-06-10
+ */
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        int water;
+       for (int i=1; i<n; i++){
+        left[i]=max (height[i-1],left[i-1]);
+       }
+        for(int i=n-2; i>=0;i--){
+            right[i]=max(height[i+1], right[i+1]);
+            }
+            for(int i=1; i<n-1; i++){
+           if(left[i]>height[i] && right[i]>height[i]){
+            water+= min(left[i], right[i])-height[i];
+           }
+            }
+       return water;
+       }
+};
