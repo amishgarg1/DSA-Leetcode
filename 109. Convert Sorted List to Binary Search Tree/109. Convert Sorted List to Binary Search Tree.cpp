396895ba617003ac8322a52fd10f7fe5d06af694
Amish Garg <amishgarg2205@gmail.com>
2026-06-11 09:22:48 +0530

    Add solution: 109. Convert Sorted List to Binary Search Tree

---
+++ 109. Convert Sorted List to Binary Search Tree/109. Convert Sorted List to Binary Search Tree.cpp
@@ -0,0 +1,54 @@
+/*
+ * Problem: 109. Convert Sorted List to Binary Search Tree
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/submissions/2029224395/
+ * Language: cpp
+ * Date: 2026-06-11
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    ListNode* findMid(ListNode* st, ListNode* end){
+        ListNode* slow= st, *fast=st;
+        while(fast!=end && fast->next!=end){
+            slow= slow->next;
+            fast=fast->next->next;
+
+        }
+        return slow;
+
+    }
+    TreeNode *listTotree(ListNode* st, ListNode* end){
+        if(st==end)
+        return nullptr;
+        ListNode* mid= findMid(st,end);
+        TreeNode* root= new TreeNode(mid->val);
+        root->left=listTotree(st, mid);
+        root->right=listTotree( mid->next, end);
+        return root;
+    }
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+       return listTotree(head, nullptr);
+    }
+};
