08c0a90a5ac93c1e62e0868e4ce31a7e8a6ba1d9
Amish Garg <amishgarg2205@gmail.com>
2026-06-12 09:51:44 +0530

    Add solution: 103. Binary Tree Zigzag Level Order Traversal

---
+++ 103. Binary Tree Zigzag Level Order Traversal/103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,55 @@
+/*
+ * Problem: 103. Binary Tree Zigzag Level Order Traversal
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/submissions/2030339980/
+ * Language: cpp
+ * Date: 2026-06-12
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==nullptr)
+        return{};
+        stack<TreeNode*> s1,s2;
+        s1.push(root);
+        while(!s1.empty() || !s2.empty()){
+            vector<int> temp;
+            while(!s1.empty()){
+                TreeNode* curr=s1.top();
+                s1.pop();
+                temp.push_back(curr->val);
+                if(curr->left)
+                s2.push(curr->left);
+                if(curr->right)
+                s2.push(curr->right);
+            }
+            ans.push_back(temp);
+            temp.clear();
+            while(!s2.empty()){
+                TreeNode* curr = s2.top();
+                s2.pop();
+                temp.push_back(curr->val);
+                if(curr->right)
+                s1.push(curr->right);
+                if(curr->left)
+                s1.push(curr->left);
+            }
+            if(temp.size()>0)
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
